35250274cc87ad3b39149da60885d432ac59f58d
Anne Urai <anne.urai@gmail.com>
2018-05-01 10:51:53 -0400

    added preprocessed and second level files

---
+++ data/second_level/fromSPSS/readSPSS.R
@@ -0,0 +1,2 @@
+library("foreign", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+dat <- read.spss("/Users/anne/Dropbox/Writing/pupil-memory/pupilsandmemory_second_level.sav")
