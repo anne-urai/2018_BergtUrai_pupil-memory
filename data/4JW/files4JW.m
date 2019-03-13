b551de897bed8a6e3a8e67e312fde5d35e492b29
Anne Urai <anne.urai@gmail.com>
2019-03-13 12:09:57 -0400

    only first row is the time axis

--- data/4JW/files4JW.m
+++ data/4JW/files4JW.m
@@ -5,9 +5,9 @@ global mypath
 
 load(sprintf('%s/data/alldata_%s.mat', mypath, 'aud'));
 writetable(dat, 'tabledata_words.csv');
-csvwrite('pupiltrials_words.csv', [pupil.time; pupil.pupil_timecourse_recog]);
+csvwrite('pupiltrials_words.csv', [mean(pupil.time); pupil.pupil_timecourse_recog]);
 
 load(sprintf('%s/data/alldata_%s.mat', mypath, 'img_raw'));
 writetable(dat, 'tabledata_img.csv');
-csvwrite('pupiltrials_img.csv', [pupil.time; pupil.pupil_timecourse_recog]);
+csvwrite('pupiltrials_img.csv', [mean(pupil.time); pupil.pupil_timecourse_recog]);
 
